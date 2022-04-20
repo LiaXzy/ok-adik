4c28b5d7011a84ab36318492b42645ded19747de
LiaXzy <102718880+LiaXzy@users.noreply.github.com>
2022-04-21 05:01:29 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -7,7 +7,7 @@ return `「 *PRICE LIST* 」
 ⬡ 2 MINGGU = Rp 8.000
 ⬡ 3 MINGGU = Rp 10.000
 ⬡ 1 BULAN = Rp 15.000
-⬡ PERMANEN = Rp 100.000
+⬡ PERMANEN = Rp 30.000
 ⬡ PERMANEN + USER PREM = Rp 150.000
 
 
@@ -17,7 +17,7 @@ return `「 *PRICE LIST* 」
 
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/6285893466306
 
 
 NOTE : 
